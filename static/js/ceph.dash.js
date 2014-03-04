a2e51c7ed8b0c71af3ddcdeaedb70f28b75cdf79
Christian Eichelmann <christian.eichelmann@1und1.de>
2014-03-04 16:02:29 +0100

    switched to chartjs for graphing and do ajax updates

---
+++ static/js/ceph.dash.js
@@ -0,0 +1,160 @@
+$(function () {
+    //
+    // Convert bytes to human readable form
+    //
+    function fmtBytes(bytes) {
+        if (bytes==0) { return "0 bytes"; }
+        var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
+        var e = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, e)).toFixed(2) + " " + s[e];
+    }
+
+    //
+    // Gauge chart configuration options
+    //
+    var gauge_options = {
+        palette: 'Soft Pastel',
+        animation: {
+            enabled: false
+        },
+        valueIndicator: {
+            type: 'triangleNeedle',
+            color: '#7a8288'
+        },
+        title: {
+             text: 'Cluster storage utilization',
+             font: { size: 18, color: '#c8c8c8', family: 'Helvetica' },
+             position: 'bottom-center'
+        },
+        geometry: {
+            startAngle: 180, 
+            endAngle: 0
+        },
+        margin: {
+            top: 0,
+            right: 10
+        },
+        rangeContainer: {
+            ranges: [
+                { startValue: 0, endValue: 60, color: '#62c462' },
+                { startValue: 60, endValue: 80, color: '#f89406' },
+                { startValue: 80, endValue: 100, color: '#ee5f5b' }
+            ]
+        },
+        scale: {
+            startValue: 0, 
+            endValue: 100,
+            majorTick: {
+                tickInterval: 20
+            },
+            label: {
+                customizeText: function (arg) {
+                    return arg.valueText + ' %';
+                }
+            }
+        }
+    };
+
+    //
+    // Pie chart configuration options
+    //
+    var chart_options = {
+        animation: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: true,
+            format:"decimal",
+            percentPrecision: 2,
+            font: { size: 14, color: '#1c1e22', family: 'Helvetica' },
+            arrowLength: 10,
+            customizeText: function() { 
+                return this.valueText + " - " + this.argumentText + " (" + this.percentText + ")";
+            }
+        },
+        customizePoint: function (point) {
+            if (point.argument.indexOf('active+clean') >= 0) {
+                return {
+                    color: '#62c462'
+                }
+            } else if (point.argument.indexOf('active') >= 0) {
+                return {
+                    color: '#f89406'
+                }
+            } else {
+                return {
+                    color: '#ee5f5b'
+                }
+            }
+        },
+        size: {
+            height: 350,
+            width: 315
+        },
+        legend: {
+            visible: false
+        },
+        series: [{
+            type: "doughnut",
+            argumentField: "state_name",
+            valueField: "count",
+            label: {
+                visible: false,
+            }
+        }]
+    };
+
+    // 
+    // GENERIC AJAX WRAPER
+    //
+    function ajaxCall(url, callback) {
+        $.ajax({
+          url: url,
+          dataType: 'json',
+          type: 'GET',
+          data: null,
+          contentType: 'application/json',
+          success: callback,
+          complete: function() {
+            setTimeout(worker, 5000);
+          }
+        });
+    }
+
+    //
+    // WORKER FUNCTION (UPDATED)
+    //
+    function worker() {
+        callback = function(data, status, xhr) {
+            bytesTotal = data['pgmap']['bytes_total'];
+            bytesUsed = data['pgmap']['bytes_used'];
+            percentUsed = Math.round((bytesUsed / bytesTotal) * 100);
+
+            // Update storage utilization gauge
+            $("#utilization").dxCircularGauge($.extend(true, {}, gauge_options, {
+                value: percentUsed
+            }));
+            $("#utilization_info").html(fmtBytes(bytesUsed) + " / " + fmtBytes(bytesTotal) + " (" + percentUsed + "%)");
+
+            // update placement group chart
+            $("#pg_status").dxPieChart($.extend(true, {}, chart_options, {
+                dataSource: data['pgmap']['pgs_by_state'],
+            }));
+            $("#pg_status_info").html(data['pgmap']['num_pgs'] + "  placementgroups in cluster");
+
+            // Update current throughput values
+            $("#operations_per_second").html(data['pgmap']['op_per_sec'] || 0);
+            $("#write_bytes").html(fmtBytes(data['pgmap']['write_bytes_sec'] || 0));
+            $("#read_bytes").html(fmtBytes(data['pgmap']['read_bytes_sec'] || 0));
+
+            // Update OSD states
+            $("#num_osds").html(data['osdmap']['osdmap']['num_osds'] || 0);
+            $("#num_in_osds").html(data['osdmap']['osdmap']['num_in_osds'] || 0);
+            $("#num_up_osds").html(data['osdmap']['osdmap']['num_up_osds'] || 0);
+            $("#unhealthy_osds").html(data['osdmap']['osdmap']['num_osds'] - data['osdmap']['osdmap']['num_up_osds'] || 0);
+        }
+
+        ajaxCall('http://poc-cephmon01.infra.server.lan:5000/', callback);
+    };
+    worker();
+})
